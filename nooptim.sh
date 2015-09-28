1f3e2a3aecf5f055f25529dc36a638abc180d0f6
bob <rlp1938@gmail.com>
2015-09-29 01:05:55 +0700

    Dist now has same set of autotools files as that made by `make dist`.

---
+++ nooptim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# nooptim.sh - build gengo with no compiler optimisations.
+
+gcc -Wall -Wextra -g -O0 -D_GNU_SOURCE=1 -c alarm.c
+gcc -Wall -Wextra -g -O0 -D_GNU_SOURCE=1 -c firstrun.c
+gcc -Wall -Wextra -g -O0 -D_GNU_SOURCE=1 -c fileops.c
+gcc -g alarm.o firstrun.o fileops.o -o alarm
